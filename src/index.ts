695425aa086a66336283a399bc277362e35ed272
hareesh <hareeshbhittam@gmail.com>
2025-08-08 00:17:28 +0530

    Minor change

--- src/index.ts
+++ src/index.ts
@@ -8,14 +8,14 @@ test("Format const declaration", () => {
   const formatted = format(tree);  
 
   assertEqual(
-    formatted,
+    formatted,  
     "const x = 5;",
     "Should format const declaration correctly",
   );
 });
 
 test("Format arrow function", () => {
-  const code = "const add = (a, b) => { return a + b; };";
+  const code = "const add = (a, b) => { return a + b; };"
   const tree = compile(code);
   const formatted = format(tree);
 
