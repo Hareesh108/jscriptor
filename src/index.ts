1249468e3c841bf60a31586c5b456dcb80a409ac
hareesh <hareeshbhittam@gmail.com>
2025-08-08 13:29:25 +0530

    Added file and path to check files in local dir

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,41 @@
+import fs from "fs";
+import path from "path";
+
 import { format } from "./formatter";
 import { compile } from "./parse";
 import { assertEqual, summarize, test } from "./test";
 
-test("Format const declaration", () => {
-  const code = "const x = 5;";
-  const tree = compile(code);
-  const formatted = format(tree);  
+// test("Format const declaration", () => {
+//   const code = "const x = 5;";
+//   const tree = compile(code);
+//   const formatted = format(tree);  
 
-  assertEqual(
-    formatted,  
-    "const x = 5;",
-    "Should format const declaration correctly",
-  );
-});
+//   assertEqual(
+//     formatted,  
+//     "const x = 5;",
+//     "Should format const declaration correctly",
+//   );
+// });
 
 test("Format arrow function", () => {
-  const code = "const add = (a, b) => { return a + b; };"
-  const tree = compile(code);
-  const formatted = format(tree);
+  // const code = "const add = (a, b) => { return a + b; };"
+  const filePath = path.join(__dirname, "example.ts");
 
-  console.log("formatted:",formatted);
+  const code = fs.readFileSync(filePath, "utf-8");
+  console.log("code",code);
+  
+  const tree = compile(code);
+
+  // const formatted = format(tree);
+
+  // console.log("formatted:",formatted);
   
 
-  assertEqual(
-    formatted,
-    "const add = (a, b) => {\n  return a + b;\n};",
-    "Should format arrow functions with proper indentation",
-  );
+  // assertEqual(
+  //   formatted,
+  //   "const add = (a, b) => {\n  return a + b;\n};",
+  //   "Should format arrow functions with proper indentation",
+  // );
 });
 
 summarize();
